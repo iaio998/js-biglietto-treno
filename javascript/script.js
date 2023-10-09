e8ab87b5e53e5f5e4cc85f081cc4fa6857061b20
iaio998 <depalatisiacopo@gmail.com>
2023-10-09 17:18:26 +0200

    Removed null breakpoint cause of added parseInt

--- javascript/script.js
+++ javascript/script.js
@@ -8,17 +8,19 @@
 let mioDiv = document.getElementById("testo");
 
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-let kilometers = prompt("Inserire numero km da percorrere.");
-let age = prompt("Inserire l'eta' del passeggero.");
+let kilometers = parseInt(prompt("Inserire numero km da percorrere."));
+let age = parseInt(prompt("Inserire l'eta' del passeggero."));
 console.log(kilometers, age);
 
 if (isNaN(kilometers) || isNaN(age)) {
   alert("Per favore inserire solo valori numerici");
   window.location.reload();
-} else if (kilometers === null || age === null) {
-  alert("Per favore inserire i valori richiesti");
-  window.location.reload();
 }
+// else if (kilometers === null || age === null) {    REMOVED DUE TO PARSEINT
+//   alert("Per favore inserire i valori richiesti");
+//   window.location.reload();
+// }
+
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 let baseTicketPrice = 0.21;
