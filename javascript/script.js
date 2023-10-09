df5a9ceb3cd289e8baad00ef6cf5bba69dda563b
iaio998 <depalatisiacopo@gmail.com>
2023-10-09 17:00:05 +0200

    Added "null" breakpoint

--- javascript/script.js
+++ javascript/script.js
@@ -15,6 +15,9 @@ console.log(kilometers, age);
 if (isNaN(kilometers) || isNaN(age)) {
   alert("Per favore inserire solo valori numerici");
   window.location.reload();
+} else if (kilometers === null || age === null) {
+  alert("Per favore inserire i valori richiesti");
+  window.location.reload();
 }
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
