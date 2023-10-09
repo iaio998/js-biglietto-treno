6082a0cd191d2ec0d59859875bc73b6f8f15e257
iaio998 <depalatisiacopo@gmail.com>
2023-10-09 15:59:54 +0200

    Added discounts %

--- javascript/script.js
+++ javascript/script.js
@@ -14,3 +14,25 @@ console.log(kilometers, age);
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+let baseTicketPrice = 0.21;
+let ticketPrice;
+
+// va applicato uno sconto del 20% per i minorenni
+if (age < 18) {
+  ticketPrice =
+    baseTicketPrice * kilometers -
+    ((baseTicketPrice * kilometers) / 100) * 20 +
+    "\u20AC";
+  console.log(ticketPrice);
+}
+// va applicato uno sconto del 40% per gli over 65.
+else if (age >= 65) {
+  ticketPrice =
+    baseTicketPrice * kilometers -
+    ((baseTicketPrice * kilometers) / 100) * 40 +
+    "\u20AC";
+  console.log(ticketPrice);
+} else {
+  ticketPrice = baseTicketPrice * kilometers;
+  console.log(ticketPrice);
+}
