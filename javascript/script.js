97c1a79ee6236ef5d87fb72d6ccda70913c2ac64
iaio998 <depalatisiacopo@gmail.com>
2023-10-09 16:14:52 +0200

    Added breakpoint on prompts

--- javascript/script.js
+++ javascript/script.js
@@ -11,7 +11,10 @@ let mioDiv = document.getElementById("testo");
 let kilometers = prompt("Inserire numero km da percorrere.");
 let age = prompt("Inserire l'eta' del passeggero.");
 console.log(kilometers, age);
-
+if (isNaN(kilometers) || isNaN(age)) {
+  let attetiont = alert("Per favore inserire solo valori numerici");
+  window.location.reload();
+}
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 let baseTicketPrice = 0.21;
@@ -33,6 +36,8 @@ else if (age >= 65) {
     "\u20AC";
   console.log(ticketPrice);
 } else {
-  ticketPrice = baseTicketPrice * kilometers;
+  ticketPrice = baseTicketPrice * kilometers + "\u20AC";
   console.log(ticketPrice);
 }
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo)
+mioDiv.innerHTML = ticketPrice;
