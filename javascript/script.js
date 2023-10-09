446a66e47319bd7df7556bebc367491a618e376e
iaio998 <depalatisiacopo@gmail.com>
2023-10-09 15:43:58 +0200

    Added prompt

--- javascript/script.js
+++ javascript/script.js
@@ -4,3 +4,13 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+let mioDiv = document.getElementById("testo");
+
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+let kilometers = prompt("Inserire numero km da percorrere.");
+let age = prompt("Inserire l'eta' del passeggero.");
+console.log(kilometers, age);
+
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
