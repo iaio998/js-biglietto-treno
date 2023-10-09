dc920b45da707e8ff7b94b1038bbc8ce9293fc52
iaio998 <depalatisiacopo@gmail.com>
2023-10-09 16:46:05 +0200

    Working exercise

--- javascript/script.js
+++ javascript/script.js
@@ -11,33 +11,28 @@ let mioDiv = document.getElementById("testo");
 let kilometers = prompt("Inserire numero km da percorrere.");
 let age = prompt("Inserire l'eta' del passeggero.");
 console.log(kilometers, age);
+
 if (isNaN(kilometers) || isNaN(age)) {
-  let attetiont = alert("Per favore inserire solo valori numerici");
+  alert("Per favore inserire solo valori numerici");
   window.location.reload();
 }
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 let baseTicketPrice = 0.21;
-let ticketPrice;
+let ticketPrice = baseTicketPrice * kilometers;
 
 // va applicato uno sconto del 20% per i minorenni
 if (age < 18) {
-  ticketPrice =
-    baseTicketPrice * kilometers -
-    ((baseTicketPrice * kilometers) / 100) * 20 +
-    "\u20AC";
+  ticketPrice = (ticketPrice - (ticketPrice / 100) * 20).toFixed(2);
   console.log(ticketPrice);
 }
 // va applicato uno sconto del 40% per gli over 65.
 else if (age >= 65) {
-  ticketPrice =
-    baseTicketPrice * kilometers -
-    ((baseTicketPrice * kilometers) / 100) * 40 +
-    "\u20AC";
+  ticketPrice = (ticketPrice - (ticketPrice / 100) * 40).toFixed(2);
   console.log(ticketPrice);
 } else {
-  ticketPrice = baseTicketPrice * kilometers + "\u20AC";
+  ticketPrice = ticketPrice.toFixed(2);
   console.log(ticketPrice);
 }
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo)
-mioDiv.innerHTML = ticketPrice;
+mioDiv.innerHTML = ticketPrice + "\u20AC";
